d0fc8f204cc1abc3905fc2a0c3058ec9bd73e516
Jayson Stemmler <jstemm@uw.edu>
2014-10-08 11:10:05 -0700

    updates to everyting, including some stats as to the display status

--- parse_log.py
+++ parse_log.py
@@ -8,7 +8,7 @@ import itertools
 import datetime
 import calendar
 import numpy
-import sys
+# import sys
 import os
 
 start_day = datetime.datetime(2014, 10, 8, 0, 0)
@@ -16,7 +16,7 @@ start_day = datetime.datetime(2014, 10, 8, 0, 0)
 def wk2num(dt):
     """take in a datetime object and return a number, 0-6.99 corresponding to day of week and time"""
     
-    import numpy
+    # import numpy
     
     wkd = dt.dayofweek
     tme = (wkd * 288) + (dt.hour * 12) + (dt.minute / 5)
@@ -26,45 +26,70 @@ def wk2num(dt):
 topdir = os.path.join(os.getenv('HOME'), '.logstats')
 dbdir = os.path.join(os.getenv('HOME'), 'Dropbox', 'workshare', 'batlog')
 
-wake = os.path.join(topdir, 'wakelog.dat')
-bat = os.path.join(topdir, 'batlog.dat')
+#log = os.path.join(topdir, 'batlog.dat')
+log = os.path.join(topdir, 'example.data')
 
-D = pd.read_table(wake, names = ['dow', 'month', 'day', 'time', 'tz', 'year', 'user', 'SSID', 'runmode'],
-                                          parse_dates = {'dtime': ['dow', 'month', 'day', 'time', 'year']},
-                                          sep = ' ', 
-                                          index_col='dtime', 
-                                          header=None)
-D = D.fillna("WiFi Disconnected")
-H = D.groupby('SSID')
+#D = pd.read_table(wake, names = ['dow', 'month', 'day', 'time', 'tz', 'year', 'user', 'SSID', 'runmode'],
+#                                          parse_dates = {'dtime': ['dow', 'month', 'day', 'time', 'year']},
+#                                          sep = ' ', 
+#                                          index_col='dtime', 
+#                                          header=None)
+#D = D.fillna("WiFi Disconnected")
+#H = D.groupby('SSID')
 
-B = pd.DataFrame(columns=['Amperage', 'Capacity', 'Current', 'Cycle Count', 'Flags', 'Voltage'])
-with open(bat) as f:
-    for c, e in itertools.izip_longest(*[f]*2):
-        a = c.split()
-        dt = a[1]+a[2].zfill(2)+a[3]+a[5]
-        d = datetime.datetime.strptime(dt, '%b%d%H:%M:%S%Y')
-        if e.startswith('    | |           "LegacyBatteryInfo"'):
-            b = eval(e.split(" = ")[1].replace("=", ":"))
-            b['Amperage'] = b['Amperage']/1e16
-            B = B.append(pd.DataFrame(b, index=[d,]))
-        else:
-            continue
+DATA = pd.DataFrame(columns=['Capacity', 'CycleCount', 'BatLife',
+                          'DevicePowerState', 'SSID', 'PCT',
+                          'WiFi', 'Eth'])
+with open(log) as f:
+    for a, b, c, d, e in itertools.izip_longest(*[f]*5):
+        # parse the date and time
+        A = a.split()
+        dt = A[1]+A[2].zfill(2)+A[3]+A[5]
+        dt = datetime.datetime.strptime(dt, '%b%d%H:%M:%S%Y')
+        
+        # parse the nework info
+        sid = b.split(' SSID ')[-1].split("\n")[0]
+        if len(sid) == 0:
+            sid = "WiFi Disconnected"
+        wifistat = b.split()[3]
+        ethstat = c.split()[3]
+        
+        # parse the battery info        
+        D = eval(d.split(" = ")[1].replace("=", ":"))
+        
+        # parse the power state
+        E = eval(e.split(" = ")[1].replace("=", ":"))
+        
+        # Create the single-row DataFrame
+        G = pd.DataFrame({'Capacity': D['Capacity'],
+                          'Current': D['Current'],
+                          'PCT' : (1.*D['Current']/D['Capacity'])*100.,
+                          'BatLife' : D['Capacity']/8440.*100.,
+                          'CycleCount': D['Cycle Count'],
+                          'DevicePowerState': E['DevicePowerState'],
+                          'SSID': sid,
+                          'WiFi': wifistat,
+                          'Eth': ethstat}, index=[dt,])
+        
+        DATA = DATA.append(G)
 
-wk = [i.dayofyear for i in D.index]
-wkbat = [i.dayofyear for i in B.index]
+AWAKE = DATA.DevicePowerState != 0
+DOY = [i.dayofyear for i in DATA.index]
 
-tod = [-1*numpy.int(numpy.floor((i.to_pydatetime() - datetime.datetime(i.year, i.month, i.day, 0, 0)).seconds / 300.)) for i in D.index]
-todbat = [-1*numpy.int(numpy.floor((i.to_pydatetime() - datetime.datetime(i.year, i.month, i.day, 0, 0)).seconds / 300.)) for i in B.index]
+TOD = [-1*numpy.int(numpy.floor(
+                                (i.to_pydatetime() - 
+                                 datetime.datetime(i.year, 
+                                                   i.month, 
+                                                   i.day, 0, 0)).seconds/300.)
+                                ) for i in DATA.index]
 
-pct = (B.Current / B.Capacity)*100.
-
-SSID_namelist = D.SSID.unique()
+SSID_namelist = DATA.SSID.unique()
 SSID_namelist.sort()
 
 nSSID = len(SSID_namelist)
 
 netnames = SSID_namelist.tolist()
-ssids = [netnames.index(i) for i in D.SSID]
+ssids = [netnames.index(i) for i in DATA.SSID]
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -74,20 +99,20 @@ import matplotlib.cm as cm
 batfig = plt.figure(figsize=(9,6), dpi=300)
 
 ax = batfig.add_subplot(211)
-ax.plot_date(B.index, B['Cycle Count'], '-')
+ax.plot_date(DATA.index, DATA['CycleCount'], '-')
 ax.set_ylabel('Cycle Count')
 ax.set_xticklabels([])
 ax.grid('on')
 ax.set_title('Battery Statistics')
 
 ax = batfig.add_subplot(212)
-ax.plot_date(B.index, (B['Capacity']/8440.)*100, 'k.')
+ax.plot_date(DATA.index, DATA['BatLife'], 'k.')
 ax.set_ylabel('Battery Capacity (%)')
 #ax.set_xticklabels([])
 ax.grid('on')
 
 #ax = batfig.add_subplot(313)
-#ax.plot_date(B.index, B['Capacity'], 'k.')
+#ax.plot_date(DATA.index, DATA['Capacity'], 'k.')
 #ax.set_ylabel('Battery Current')
 #ax.grid('on')
 
@@ -98,7 +123,8 @@ plt.savefig(os.path.join(dbdir, 'batstats.png'), dpi=300)
 batyear = plt.figure(figsize=(10, 5), dpi=300)
 ax = batyear.add_axes([0.12, 0.1, 0.9, 0.8])
 
-c = ax.scatter(wkbat, todbat, c=pct, edgecolor='none', marker='s', s=1, cmap=cm.rainbow_r, vmax=100., vmin=0.)
+c = ax.scatter(DOY, TOD, c=DATA['PCT'], edgecolor='none', marker='s', s=1, 
+               cmap=cm.rainbow_r, vmax=100., vmin=0.)
 
 ax.set_xlabel('Day of Year')
 ax.set_ylabel('Time of Day')
@@ -121,7 +147,8 @@ plt.savefig(os.path.join(dbdir, 'batyear.png'), dpi=300)
 fig = plt.figure(figsize=(10,5), dpi=300)
 ax = fig.add_subplot(111)
 
-c=ax.scatter(wk, tod, c=numpy.array(ssids), edgecolor='none', marker='s', s=1, cmap=cm.rainbow)
+c=ax.scatter(DOY, TOD, c=numpy.array(ssids), edgecolor='none', marker='s', 
+             s=1, cmap=cm.rainbow)
 ax.set_xlabel('Day of Year')
 ax.set_ylabel('Time of Day')
 
@@ -146,12 +173,16 @@ plt.savefig(os.path.join(dbdir, 'yearview.png'), dpi=300)
 fig.clf()
 ax = fig.add_axes([0.05, 0.1, 0.7, 0.8])
 
-counts = [wk2num(g) for g in D.index]
-ax.hist(counts, bins=numpy.arange(0, 2016), histtype='step', label='ALL')
+histbins = numpy.arange(0, 2016, 3)
+
+counts = [wk2num(g) for g in DATA.index]
+ax.hist(counts, bins=histbins, histtype='step', label='ALL')
+
+H = DATA[AWAKE].groupby('SSID')
 
 for g in H.groups.keys():
     counts = [wk2num(v) for v in H.get_group(g).index]
-    ax.hist(counts, bins=numpy.arange(0, 2016), histtype='step', label=g)
+    ax.hist(counts, bins=histbins, histtype='step', label=g)
 
 ax.set_xticks(numpy.arange(7)*288)
 ax.set_xticks((numpy.arange(7)+.5)*288, minor=True)
@@ -161,7 +192,8 @@ ax.set_xlim((0, 2016))
 ax.set_yticks([])
 ax.set_ylim(0, ax.get_ylim()[1]*1.1)
 
-ax.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=10, title="SSID", frameon=False)
+ax.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=10, 
+          title="SSID", frameon=False)
 
 ax.set_xlabel('Day of Week')
 ax.set_ylabel('Computer Useage')
