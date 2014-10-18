393143776e8a8671dfbb9c681e9efe7afac87f9a
Jayson Stemmler <jstemm@uw.edu>
2014-10-17 17:19:05 -0700

    made some large improvements to the plotting

--- parse_log.py
+++ parse_log.py
@@ -23,6 +23,31 @@ def wk2num(dt):
     
     return tme
 
+def sid_check(sid):
+    
+    if len(sid) == 0:
+        return 'No WiFi'
+    
+    home = ['DangerZone', 'Danger Zone', 'Office Zone']
+    work = ['University of Washington']
+    coffee = ['IssaquahCoffee', 'Allegro', 'Google Starbucks']
+    hotspot = ['JStemm']
+    
+    if sid in home:
+        return 'Home'
+        
+    elif sid in work:
+        return 'Work'
+        
+    elif sid in coffee:
+        return 'Coffee Shop'
+        
+    elif sid in hotspot:
+        return 'Mobile Hotspot'
+        
+    else:
+        return 'Other'
+
 topdir = os.path.join(os.getenv('HOME'), '.logstats')
 dbdir = os.path.join(os.getenv('HOME'), 'Dropbox', 'workshare', 'batlog')
 
@@ -48,9 +73,7 @@ with open(log) as f:
         dt = datetime.datetime.strptime(dt, '%b%d%H:%M:%S%Y')
         
         # parse the nework info
-        sid = b.split(' SSID ')[-1].split("\n")[0]
-        if len(sid) == 0:
-            sid = "WiFi Disconnected"
+        sid = sid_check(b.split(' SSID ')[-1].split("\n")[0])
         wifistat = b.split()[3]
         C = c.split()
         if len(C) < 2:
@@ -62,7 +85,10 @@ with open(log) as f:
         D = eval(d.split(" = ")[1].replace("=", ":"))
         
         # parse the power state
-        E = eval(e.split(" = ")[1].replace("=", ":"))
+        if len(e) < 7:
+            E = {'CurrentPowerState': 0}
+        else:
+            E = eval(e.split(" = ")[1].replace("=", ":"))
         
         # Create the single-row DataFrame
         G = pd.DataFrame({'Capacity': D['Capacity'],
@@ -77,15 +103,12 @@ with open(log) as f:
         
         DATA = DATA.append(G)
 
-AWAKE = DATA.CurrentPowerState != 0
-DOY = [i.dayofyear for i in DATA.index]
+AWAKE = DATA[DATA.CurrentPowerState != 0]
 
-TOD = [-1*numpy.int(numpy.floor(
-                                (i.to_pydatetime() - 
-                                 datetime.datetime(i.year, 
-                                                   i.month, 
-                                                   i.day, 0, 0)).seconds/300.)
-                                ) for i in DATA.index]
+nDays = (DATA.index[-1] - DATA.index[0]).days + 1
+nTimeOfDay = (60./5)*24 # number of 5 minute samples per day
+
+startDate = DATA.index[0].date()
 
 SSID_namelist = DATA.SSID.unique()
 SSID_namelist.sort()
@@ -94,6 +117,25 @@ nSSID = len(SSID_namelist)
 
 netnames = SSID_namelist.tolist()
 ssids = [netnames.index(i) for i in DATA.SSID]
+
+DATA['ssids'] = ssids
+
+batgrid = numpy.zeros((nTimeOfDay, nDays))*numpy.nan
+ssidgrid = numpy.zeros_like(batgrid)*numpy.nan
+awakegrid = numpy.zeros_like(batgrid)*numpy.nan
+
+for i in DATA.iterrows():
+    dS = (i[0].date() - startDate).days
+    tOD = numpy.int(numpy.floor(
+           (i[0].to_pydatetime() - 
+            datetime.datetime(i[0].year, i[0].month, i[0].day)).seconds/300.))
+    
+    batgrid[tOD, dS] = i[1].PCT
+    ssidgrid[tOD, dS] = i[1].ssids
+    
+    if i[1].CurrentPowerState !=0:
+        awakegrid[tOD, dS] = 1
+
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -127,16 +169,19 @@ plt.savefig(os.path.join(dbdir, 'batstats.png'), dpi=300)
 batyear = plt.figure(figsize=(10, 5), dpi=300)
 ax = batyear.add_axes([0.12, 0.1, 0.9, 0.8])
 
-c = ax.scatter(DOY, TOD, c=DATA['PCT'], edgecolor='none', marker='s', s=1, 
-               cmap=cm.rainbow_r, vmax=100., vmin=0.)
+#c = ax.scatter(DOY, TOD, c=DATA['PCT'], edgecolor='none', marker='s', s=1, 
+#               cmap=cm.rainbow_r, vmax=100., vmin=0.)
 
-ax.set_xlabel('Day of Year')
+c = ax.imshow(batgrid, vmin=0., vmax=100, aspect='auto', interpolation='none',
+                cmap=cm.jet_r)
+
+ax.set_xlabel('Days')
 ax.set_ylabel('Time of Day')
 
-ax.set_xlim((0, 366))
-ax.set_ylim((-288, 0))
+#ax.set_xlim((0, 366))
+#ax.set_ylim((-288, 0))
 
-ax.set_yticks(numpy.arange(0, 25, 3)*-12)
+ax.set_yticks(numpy.arange(0, 25, 3)*12)
 ax.set_yticklabels(['Midnight', '3am', '6am', '9am', 'Noon', '3pm', '6pm', '9pm', 'Midnight'])
 
 ax.grid('on')
@@ -151,15 +196,18 @@ plt.savefig(os.path.join(dbdir, 'batyear.png'), dpi=300)
 fig = plt.figure(figsize=(10,5), dpi=300)
 ax = fig.add_subplot(111)
 
-c=ax.scatter(DOY, TOD, c=numpy.array(ssids), edgecolor='none', marker='s', 
-             s=1, cmap=cm.rainbow)
+#c=ax.scatter(DOY, TOD, c=numpy.array(ssids), edgecolor='none', marker='s', 
+#             s=1, cmap=cm.rainbow)
+
+c = ax.imshow(ssidgrid, interpolation='none', aspect='auto', cmap=cm.rainbow_r)
+
 ax.set_xlabel('Day of Year')
 ax.set_ylabel('Time of Day')
 
-ax.set_xlim((0, 366))
-ax.set_ylim((-288, 0))
+#ax.set_xlim((0, 366))
+#ax.set_ylim((-288, 0))
 
-ax.set_yticks(numpy.arange(0, 25, 3)*-12)
+ax.set_yticks(numpy.arange(0, 25, 3)*12)
 ax.set_yticklabels(['Midnight', '3am', '6am', '9am', 'Noon', '3pm', '6pm', '9pm', 'Midnight'])
 
 ax.grid('on')
@@ -175,19 +223,19 @@ plt.savefig(os.path.join(dbdir, 'yearview.png'), dpi=300)
 # weekstats
 #################################################
 fig.clf()
-ax = fig.add_axes([0.05, 0.1, 0.7, 0.8])
-
+#ax = fig.add_axes([0.05, 0.1, 0.7, 0.8])
+ax = fig.add_subplot(111)
 histbins = numpy.arange(0, 2016, 3)
 
 counts = [wk2num(g) for g in DATA.index]
 ax.hist(counts, bins=histbins, histtype='step', label='ALL')
 
-H = DATA[AWAKE].groupby('SSID')
-#H = DATA.groupby('SSID')
+#H = AWAKE.groupby('SSID')
+##H = DATA.groupby('SSID')
 
-for g in H.groups.keys():
-    counts = [wk2num(v) for v in H.get_group(g).index]
-    ax.hist(counts, bins=histbins, histtype='step', label=g)
+#for g in H.groups.keys():
+#    counts = [wk2num(v) for v in H.get_group(g).index]
+#    ax.hist(counts, bins=histbins, histtype='step', label=g)
 
 ax.set_xticks(numpy.arange(7)*288)
 ax.set_xticks((numpy.arange(7)+.5)*288, minor=True)
@@ -197,12 +245,13 @@ ax.set_xlim((0, 2016))
 ax.set_yticks([])
 ax.set_ylim(0, ax.get_ylim()[1]*1.1)
 
-ax.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=10, 
-          title="SSID", frameon=False)
+#ax.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=10, 
+#          title="SSID", frameon=False)
 
+ax.grid('on', which='major')
 ax.set_xlabel('Day of Week')
 ax.set_ylabel('Computer Useage')
-plt.suptitle('Computer Useage by Day of Week and SSID')
+#plt.suptitle('Computer Useage by Day of Week and SSID')
 
 plt.savefig(os.path.join(dbdir, 'weekstats.png'), dpi=300)
 
